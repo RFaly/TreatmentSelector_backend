96ce1b8b61e86ed26a9660de9bfc6ee18b597855
RFaly <ramahafaly2.0@gmail.com>
2020-12-09 20:26:28 +0300

    create my mutations

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,84 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    
+    #/patients
+    field :patients, [Types::PatientType], null: false
+    def patients
+      Patient.all
     end
+
+    #/patients/:id
+    field :patient, Types::PatientType, null: false do
+      argument :id, ID, required: true
+    end
+    def patient(id:)
+      Patient.find(id)
+    end 
+
+    #/traitment_categories
+    field :traitment_categories, [Types::TraitmentCategoryType], null: false
+    def traitment_categories
+      TraitmentCategory.all
+    end
+
   end
 end
+
+=begin
+Liste de tous les traitement et category
+{
+  traitmentCategories{
+    id,
+    nameEn,
+    nameFr,
+    nameMg,
+    traitments {
+      id,
+      nameEn,
+      nameFr,
+      nameMg
+    }
+  }
+}
+
+Liste des patients et les traitment + category 
+{
+  patients {
+    id,
+    name,
+    traitments{
+      id,
+      nameEn,
+      nameFr,
+      nameMg,
+      traitmentCategory {
+        id,
+        nameEn,
+        nameFr,
+        nameMg
+      }
+    }
+  }
+}
+
+Liste de traitment d'une patien
+{
+  patient(id:2) {
+    id,
+    name,
+    traitments{
+      id,
+      nameEn,
+      nameFr,
+      nameMg,
+      traitmentCategory {
+        id,
+        nameEn,
+        nameFr,
+        nameMg
+      }
+    }
+  }
+}
+
+=end
