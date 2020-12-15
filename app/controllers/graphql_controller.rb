3fb3779c51936dc54589e029d2de166d7069f5e4
RFaly <ramahafaly2.0@gmail.com>
2020-12-15 10:50:08 +0300

    autorization okk

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,7 +3,7 @@ class GraphqlController < ApplicationController
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
-  before_action :authorized
+  before_action :authorized, only: [:auto_login]
   
   def execute
     variables = prepare_variables(params[:variables])
